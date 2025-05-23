d3eccdd2dfb645da17caf2cd6f4b71abc04bfec2
Eileen-10 <93325000+Eileen-10@users.noreply.github.com>
2025-05-24 01:10:26 +0800

    Removed model file and commented all classification function call

--- backend/src/model/ques_ClassificationModel.py
+++ backend/src/model/ques_ClassificationModel.py
@@ -1,14 +1,14 @@
-import torch.nn as nn
-from transformers import AutoModel
+# import torch.nn as nn
+# from transformers import AutoModel
 
-class ClassificationModel(nn.Module):
-    def __init__(self, num_classes):
-        super(ClassificationModel, self).__init__()
-        self.bert = AutoModel.from_pretrained("bert-base-uncased")
-        self.classifier = nn.Linear(self.bert.config.hidden_size, num_classes)
+# class ClassificationModel(nn.Module):
+#     def __init__(self, num_classes):
+#         super(ClassificationModel, self).__init__()
+#         self.bert = AutoModel.from_pretrained("nreimers/MiniLM-L6-H384-uncased")
+#         self.classifier = nn.Linear(self.bert.config.hidden_size, num_classes)
 
-    def forward(self, input_ids, attention_mask=None, token_type_ids=None):
-        outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
-        cls_output = outputs.last_hidden_state[:, 0, :]  # [CLS] token
-        logits = self.classifier(cls_output)
-        return logits
+#     def forward(self, input_ids, attention_mask=None, token_type_ids=None):
+#         outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
+#         cls_output = outputs.last_hidden_state[:, 0, :]  # [CLS] token
+#         logits = self.classifier(cls_output)
+#         return logits
